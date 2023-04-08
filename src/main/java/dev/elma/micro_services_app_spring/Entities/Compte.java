2762ee2f4b536813821080b7f3c9f52c71bc9dda
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-08 18:04:40 +0000

    commit

--- src/main/java/dev/elma/micro_services_app_spring/Entities/Compte.java
+++ src/main/java/dev/elma/micro_services_app_spring/Entities/Compte.java
@@ -2,18 +2,20 @@ package dev.elma.micro_services_app_spring.Entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.jcp.xml.dsig.internal.dom.DOMUtils;
 
 @Data @AllArgsConstructor @NoArgsConstructor
 
 @Entity
 public class Compte {
-    @Id @GeneratedValue
-    private double id;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String password;
-    private long solde;
+    private double solde;
 }
--- src/main/java/dev/elma/micro_services_app_spring/MicroServicesAppSpringApplication.java
+++ src/main/java/dev/elma/micro_services_app_spring/MicroServicesAppSpringApplication.java
@@ -1,18 +1,22 @@
 package dev.elma.micro_services_app_spring;
 
+import dev.elma.micro_services_app_spring.Dao.CompteRepository;
+import dev.elma.micro_services_app_spring.Entities.Compte;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MicroServicesAppSpringApplication implements CommandLineRunner {
-
+	@Autowired
+	CompteRepository compteRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServicesAppSpringApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		compteRepository.save(new Compte(null,"Abdeljalil","1234",12200));
 	}
 }
