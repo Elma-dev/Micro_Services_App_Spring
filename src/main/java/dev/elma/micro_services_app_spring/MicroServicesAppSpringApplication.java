4269729a667d59e6d16302fbfc12158d85923bc2
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-08 17:53:12 +0000

    commit

--- src/main/java/dev/elma/micro_services_app_spring/MicroServicesAppSpringApplication.java
+++ src/main/java/dev/elma/micro_services_app_spring/MicroServicesAppSpringApplication.java
@@ -1,13 +1,18 @@
 package dev.elma.micro_services_app_spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroServicesAppSpringApplication {
+public class MicroServicesAppSpringApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServicesAppSpringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
