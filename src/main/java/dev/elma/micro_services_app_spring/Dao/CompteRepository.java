2f074f1930e04265cd68020c6f23f6a5ee573245
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-08 17:59:24 +0000

    commit

---
+++ src/main/java/dev/elma/micro_services_app_spring/Dao/CompteRepository.java
@@ -0,0 +1,7 @@
+package dev.elma.micro_services_app_spring.Dao;
+
+import dev.elma.micro_services_app_spring.Entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompteRepository extends JpaRepository<Compte,Long> {
+}
