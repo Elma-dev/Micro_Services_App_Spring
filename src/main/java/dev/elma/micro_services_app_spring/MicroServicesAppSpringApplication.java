5ad0a4ef42861785886bc466c46d9c13b7d8294c
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-08 17:50:49 +0000

    Initial commit

---
+++ src/main/java/dev/elma/micro_services_app_spring/MicroServicesAppSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.micro_services_app_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicesAppSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/elma/micro_services_app_spring/MicroServicesAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.micro_services_app_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicesAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
