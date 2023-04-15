90d6018710f0f1c43c72ebe700df7c8630852db0
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-15 00:47:46 +0000

    commit

---
+++ src/main/java/dev/elma/micro_services_app_spring/web/CompteRestControllerWithServices.java
@@ -0,0 +1,52 @@
+package dev.elma.micro_services_app_spring.web;
+
+import dev.elma.micro_services_app_spring.dao.CompteRepository;
+import dev.elma.micro_services_app_spring.dtos.CompteDTORequest;
+import dev.elma.micro_services_app_spring.dtos.CompteDTOResponse;
+import dev.elma.micro_services_app_spring.entities.Compte;
+import dev.elma.micro_services_app_spring.services.BankServices;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/apiServices")
+@NoArgsConstructor @AllArgsConstructor @Data @Builder
+public class CompteRestControllerWithServices {
+    CompteRepository compteRepository;
+    BankServices bankServices;
+
+    @GetMapping("/ClientComptes")
+    List<Compte> getAllComptes(){
+        return compteRepository.findAll();
+    }
+    @PostMapping("/newClientCompte")
+    CompteDTOResponse newClientCompte(@RequestBody CompteDTORequest compte ){
+        System.out.println(compte);
+        CompteDTOResponse compteDTOResponse = bankServices.addNewAccount(compte);
+        return compteDTOResponse;
+    }
+    @PutMapping("/updateCompte/{id}")
+    Compte updateCompte(@PathVariable String id,@RequestBody Compte compte){
+        Compte cmpt=compteRepository.findById(id).orElseThrow();
+        if(!compte.getPassword().isEmpty()){
+            cmpt.setPassword(compte.getPassword());
+        }
+        if(compte.getSolde()!=0){
+            cmpt.setSolde(compte.getSolde());
+        }
+        return compteRepository.save(cmpt);
+    }
+    @DeleteMapping("/deleteCompte/{id}")
+    Compte deleteCompte(@PathVariable String id){
+        Compte compte=compteRepository.findById(id).orElseThrow();
+        compteRepository.delete(compte);
+        return compte;
+    }
+}
