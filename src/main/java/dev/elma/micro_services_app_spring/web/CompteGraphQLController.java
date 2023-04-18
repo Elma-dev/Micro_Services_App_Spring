98f56d2f60febea70d78bd5770055c7715baee74
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-18 00:34:49 +0000

    commit

---
+++ src/main/java/dev/elma/micro_services_app_spring/web/CompteGraphQLController.java
@@ -0,0 +1,29 @@
+package dev.elma.micro_services_app_spring.web;
+
+import dev.elma.micro_services_app_spring.dao.CompteRepository;
+import dev.elma.micro_services_app_spring.entities.Compte;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.Arguments;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class CompteGraphQLController {
+    private CompteRepository compteRepository;
+
+    @QueryMapping
+    public List<Compte> comptes(){
+        return compteRepository.findAll();
+    }
+    @QueryMapping
+    public Compte compte(@Argument String id){
+        return compteRepository.findById(id).get();
+    }
+
+
+}
